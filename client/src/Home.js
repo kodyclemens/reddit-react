cfb4020a97972d838294bbeec334532f903d9f40
kodyclemens <kody.clemens@gmail.com>
2019-03-29 13:58:59 -0400

    adjusting routing to be handled by React, not Rails. Added mock components to test routing

---
+++ client/src/Home.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+  render() {
+    return(
+      <div>
+        You are home.
+      </div>
+    )
+  };
+};
+
+export default Home
