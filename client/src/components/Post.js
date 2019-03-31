eb933458c327387f27537dcdf8494075e70aa731
kodyclemens <kody.clemens@gmail.com>
2019-03-31 18:37:25 -0400

    persisted post IDs are gathered in the Posts component and passed down to individual Post(s). If a Post's ID matches any of the persisted IDs, the Post will not render a pin button. Changed styling if pin button to be more intuitive to users

--- client/src/components/Post.js
+++ client/src/components/Post.js
@@ -1,20 +1,36 @@
 import React, { PureComponent } from 'react';
-import { Card, Image } from 'react-bootstrap';
+import { Card, Image, Button } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { connect } from 'react-redux';
 import { persistPost } from '../actions/index';
 
 class Post extends PureComponent {
+
+  handleClick = (event, post)=> {
+    this.props.persistPost(post);
+    event.target.disabled = true;
+    event.target.className = "btn btn-success";
+    event.target.innerText = "Pinned to Frontpage"
+  }
+
   render() {
     const post = this.props.post;
+    const persistedPostIDs = this.props.persistedPosts;
     const userLink = "https://old.reddit.com/user/" + post.author;
     post.perma_link = "https://old.reddit.com" + post.perma_link;
     let pinAction = ''
 
-    // Do not show the bookmark visual if the post has already been persisted to the DB
-    if (post.persisted !== true) {
-      pinAction = <FontAwesomeIcon icon={['far', 'bookmark']}
-      transform="right-5" onClick={(event) => this.props.persistPost(post)} />
+    // If the post ID from the search results matches an already persisted post's ID
+    // we do not want to show the pin button (logic for search results feed)
+    if (persistedPostIDs.includes(post.id)) {
+      post.persisted = true;
+    }
+
+    // Do not show the pin button if the post has already been persisted to the DB
+    // Each persisted post has a 'persisted' property set to true
+    // This is done in the posts_reducer when SET_PERSISTED is called
+    if (post.persisted !== true ) {
+      pinAction = <Button variant="outline-success" onClick={(event) => this.handleClick(event, post)}>Pin to Frontpage</Button>
     }
 
     return(
@@ -22,7 +38,9 @@ class Post extends PureComponent {
       <Card>
         <Card.Body>
           <Card.Header className="text-left">
-            Posted by <a href={userLink}>{post.author}</a>{pinAction}
+            Posted by <a href={userLink}>{post.author}</a>
+            <br></br>
+            {pinAction}
           </Card.Header>
           <Card.Title className="text-left">{post.title}</Card.Title>
           <Image src={post.image || post.url} rounded fluid />
--- client/src/components/Posts.js
+++ client/src/components/Posts.js
@@ -1,13 +1,25 @@
 import React, { PureComponent } from 'react';
 import {LoadingSpinner} from './Loading';
 import Post from './Post';
+import { connect } from 'react-redux';
+import { setPersisted } from '../actions/index';
 
 class Posts extends PureComponent {
+
+  componentDidMount() {
+    // User could come directly to the search page, bypassing the setPersisted on the homepage
+    // We also want to update persisted posts when the component mounts to be as up-to-date as possible with the database
+    // Uncomment the if statement to only setPersisted if the prop array is empty (reduces DB calls, performance increase)
+
+    // if (this.props.persistedPostIDs.length <= 0) {
+      this.props.dispatch(setPersisted());
+    // }
+  }
 
   render() {
     let renderPosts = [];
     if (this.props.posts !== undefined) {
-      renderPosts = this.props.posts.map((post, id) => {return <Post key={id} post={post} handleClick={this.handleClick} />})
+      renderPosts = this.props.posts.map((post, id) => {return <Post key={id} post={post} handleClick={this.handleClick} persistedPosts={this.props.persistedPostIDs} />})
     }
 
     if (this.props.loading === true) {
@@ -27,4 +39,16 @@ class Posts extends PureComponent {
   }
 };
 
-export default Posts;
+const mapStateToProps = state => {
+  if (state.posts.persisted) {
+    const persistedPosts = state.posts.persisted;
+    let persistedPostIDs = [];
+    for(let i in persistedPosts) {
+      persistedPostIDs.push(persistedPosts[i].post_id)
+    };
+    return {persistedPostIDs: persistedPostIDs}
+  };
+  return {persistedPostIDs: []}
+}
+
+export default connect(mapStateToProps)(Posts);
