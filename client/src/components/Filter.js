170490d361215ce4fab55ee1edc406e788afbcd3
kodyclemens <kody.clemens@gmail.com>
2019-04-03 23:44:10 -0400

    styling of filter buttons

--- client/src/components/Filter.js
+++ client/src/components/Filter.js
@@ -3,7 +3,7 @@ import { ButtonToolbar, Button} from 'react-bootstrap';
 
 const Filter = (props) => {
   return(
-    <div>
+    <div id="filter-btns">
     <ButtonToolbar>
       <Button href="#" className="filter-btn active-filter" name="recently-voted" onClick={event => props.handleFilter(event)}>Recently Voted</Button>
       <Button href="#" className="filter-btn" name="most-cheers" onClick={event => props.handleFilter(event)}>Most Cheers</Button>
