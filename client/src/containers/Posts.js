30682bfd6b3c677e1bedc4c6674dfb10b1ac466c
kodyclemens <kody.clemens@gmail.com>
2019-04-05 15:23:48 -0400

    prevent persisted posts from being queried every time the search page is visited, as long as they are already present in state.

--- client/src/containers/Posts.js
+++ client/src/containers/Posts.js
@@ -8,15 +8,12 @@ class Posts extends PureComponent {
 
   
   componentDidMount() {
-    // User could come directly to the search page, bypassing the setPersisted on the homepage
-    // We also want to update persisted posts when the component mounts to be as up-to-date as possible with the database
-    // Uncomment the if statement to only setPersisted if the prop array is empty (reduces DB calls, performance increase)
-    
-    // let filter = this.handleFilter();
+    // User could come directly to the search page, bypassing the setPersisted on the homepage, resulting in pin buttons for already persisted posts.
+    // Prevent unnescessary DB queries if persisted posts are already present    
 
-    // if (this.props.persistedPostIDs.length <= 0) {
+    if (this.props.persistedPostIDs.length <= 0) {
       this.props.dispatch(setPersisted());
-      // }
+      }
     }
     
     render() {
